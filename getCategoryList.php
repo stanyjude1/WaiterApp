63944d8234127ed851ee87ca584be0370b838e6d
Stany Jude <stanyjude@kwiqpick.com>
2019-02-26 16:43:45 +0530

    getCategoryList POC completed

---
+++ getCategoryList.php
@@ -0,0 +1,38 @@
+<?php
+
+$row = 1;
+$original_arr= array();
+$cat = array();
+
+if(isset($_REQUEST['staff_id'])){
+    if (($handle = fopen("category.csv", "r")) !== FALSE) {
+        while (($data = fgetcsv($handle)) !== FALSE) {
+            $num = count($data);
+            $row++;
+            array_push($original_arr, $data);
+            array_push($cat, $data[0]);
+            
+        }
+        fclose($handle);
+        $cat=array_unique($cat);
+        $temp=array();
+        
+        for($i=1;$i<sizeof($cat);$i++)
+        {
+
+          $temp[]=["id"=>(string)$i,"category"=>$cat[$i]];
+        }
+
+        $response=json_encode(array('response'=>200,"FoodItemCategory"=>$temp));
+        echo $response;
+    }
+}
+else{
+    $response['error_code'] = 3;
+    $response['message'] = 'staff_id is missing';
+    echo json_encode($response,JSON_UNESCAPED_SLASHES);
+}
+
+exit();
+
+?>
