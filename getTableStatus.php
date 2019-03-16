3f511b3ece8670ead27890dd4ae90e8bbc6cff65
Stany Jude <stanyjude@kwiqpick.com>
2019-03-16 18:36:53 +0530

    Added table status

--- getTableStatus.php
+++ getTableStatus.php
@@ -1,33 +1,26 @@
 <?php
 
-echo "{
-    'DiningDetails': [
-        {
-            'table_name': 'TBL 11',
-            'table_id': 11,
-            'capacity': 4,
-            'status': 'Active',
-            'billamount':300.00
-        },
-        {
-            'table_name': 'TBL 12',
-            'table_id': 12,
-            'capacity': 4,
-            'status': 'Active',
-            'billamount':300.00
-
-        },
-        {
-            'table_name': 'TBL 13',
-            'table_id': 13,
-            'capacity': 4,
-            'status': 'Active',
-            'billamount':300.00
-        }
-    ],
-    'response': 200,
-    'restaurant_id': 1,
-    'restaurant_name': 'Karama Restaurant'
-}";
+if(!isset($_REQUEST['staff_id'])){
+    $response["DiningDetails"] = array();
+    for ($j=0; $j < 10 ; $j++) { 
+        $names = range("A","Z");
+        $ids = range(1,100);
+        $table_details[$j]["table_name"] = "TBL ".$names[$j];
+        $table_details[$j]["table_id"]   = (int)($j+1);
+        $table_details[$j]["capacity"] = (int)rand(4,10);
+        $table_details[$j]["status"]     = "Active";
+        $table_details[$j]["billamount"] = (float)number_format(mt_rand(3000,10000)/10, 2, '.', '');
+    }
+    $response["DiningDetails"] = $table_details;
+    $response["response"] = 200;
+    $response["restaurant_id"] = "1";
+    $response["restaurant_name"] = "Karama Restaurant";
+}
+else{
+    $response['error_code'] = 3;
+    $response['message'] = 'staff_id is missing';
+}
+echo json_encode($response,JSON_UNESCAPED_SLASHES);
+exit();
 
 ?>
