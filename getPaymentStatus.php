8fc348b27aeace2fc4092850618639571ff522b4
Stany Jude <stanyjude@kwiqpick.com>
2019-03-17 13:32:39 +0530

    Added payment

---
+++ getPaymentStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_REQUEST['staff_id'])){
+    //$response["OrderDetails"] = array();
+    $response["success"] = 0;
+    $response["message"] = "paymentsuceess";
+}
+else{
+    $response['error_code'] = 3;
+    $response['message'] = 'staff_id is missing';
+}
+echo json_encode($response,JSON_UNESCAPED_SLASHES);
+exit();
+
+?>
