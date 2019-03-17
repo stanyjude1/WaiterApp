75219dd73b2e1944d84e30e1c5d7bb09fac8ee9a
Stany Jude <stanyjude@kwiqpick.com>
2019-03-17 11:43:37 +0530

    Added sample bill wihtout array

--- getBillDetailsTable.php
+++ getBillDetailsTable.php
@@ -1,28 +1,29 @@
 <?php
 
-if(isset($_REQUEST['staff_id'])){
-    $response["OrderDetails"] = array();
-    for ($j=0; $j < 1 ; $j++) { 
-        $names = range("A","Z");
-        $ids = range(1,100);
-        $table_details[$j]["table_name"] = "TBL ".$names[$j];
-        $table_details[$j]["table_id"]   = (int)($j+1);
-        $table_details[$j]["ordered_categories"] = "Starters";
-        $table_details[$j]["ordered_items"] = "Chicken Kebab";
-        $table_details[$j]["ordered_items_qty"]  = "1";
-        $table_details[$j]["items_price"] = "150";
-        $table_details[$j]["total_price"] = "1050";
-        $table_details[$j]["total_gst"] = "55";
-        $table_details[$j]["payable_bill"] = "1105";
-        $table_details[$j]["order_status"] = "confired";
-        $table_details[$j]["payment_status"] = "waiting";
-        $table_details[$j]["ordered_time"] = "12:30";
-        $table_details[$j]["date"] = "21-12-2019";
-    }
-    $response["OrderDetails"] = $table_details;
+if(!isset($_REQUEST['staff_id'])){
+    //$response["OrderDetails"] = array();
     $response["response"] = 200;
     $response["restaurant_id"] = "1";
     $response["restaurant_name"] = "Karama Restaurant";
+    for ($j=0; $j < 1 ; $j++) { 
+        $names = range("A","Z");
+        $ids = range(1,100);
+        $response["table_name"] = "TBL ".$names[$j];
+        $response["table_id"]   = (int)($j+1);
+        $response["ordered_categories"] = "Starters";
+        $response["ordered_items"] = "Chicken Kebab";
+        $response["ordered_items_qty"]  = "1";
+        $response["items_price"] = "150";
+        $response["total_price"] = "1050";
+        $response["total_gst"] = "55";
+        $response["payable_bill"] = "1105";
+        $response["order_status"] = "confired";
+        $response["payment_status"] = "waiting";
+        $response["ordered_time"] = "12:30";
+        $response["date"] = "21-12-2019";
+    }
+    //$response["OrderDetails"] = $table_details;
+    
 }
 else{
     $response['error_code'] = 3;
